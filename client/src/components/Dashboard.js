e29be0bf11b0d63465d00acdacd10d453c46936f
Nawsheen Mehereen <nawsheen@iut-dhaka.edu>
2023-05-26 21:04:12 +0800

    dashboard redirects fixed

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -1,6 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router';
+const Dashboard = ({ setAuth }) => {
 
-const Dashboard = () => {
+  const navigate = useNavigate();
+
+  const checkAuthenticated = async () => {
+    try {
+      const res = await fetch("http://localhost:5000/auth/is-verify", {
+        method: "GET",
+        headers: { token: localStorage.token }
+      });
+
+      const parseRes = await res.json();
+      
+      parseRes === true ? setIsAuthenticated(true) :navigate("/");
+      
+     
+      
+    } catch (err) {
+      console.error(err.message);
+    }
+  };
+  
+
+  useEffect(() => {
+    checkAuthenticated();
+  }, []);
+  
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  
   return <h2>Dashboard</h2>;
 };
 
