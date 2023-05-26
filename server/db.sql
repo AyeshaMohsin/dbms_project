dcaa78afe281111dcb0b2d405e1ef1a9f1f089cb
Nawsheen Mehereen <nawsheen@iut-dhaka.edu>
2023-05-27 01:29:09 +0800

    added create table commands

--- server/db.sql
+++ server/db.sql
@@ -31,3 +31,61 @@ create table customer (
     customer_password varchar(255) not null,
     customer_address address
 );
+
+
+create table seller (
+    seller_id uuid primary key default uuid_generate_v4(),
+    seller_name varchar(255) not null,
+    seller_email varchar(255) not null,
+    seller_password varchar(255) not null,
+    seller_isVerified boolean default false
+);
+
+create type contact_info as (
+    email varchar(255),
+    phone varchar(255)
+);
+
+create table store (
+    store_id uuid primary key default uuid_generate_v4(),
+    store_name varchar(255) not null,
+    store_address address not null,
+    store_contact contact_info not null,
+    store_isVerified boolean default false,
+    store_seller_id uuid not null,
+    foreign key(store_seller_id) references seller(seller_id)
+);
+
+
+create table generic_medicine(
+    generic_medicine_id uuid primary key default uuid_generate_v4(),
+    generic_medicine_name varchar(255) not null,
+    generic_medicine_main_inegredient varchar(255),
+    generic_medicine_side_effects varchar(600)[]
+);
+
+create table manufacturer(
+    manufacturer_id uuid primary key default uuid_generate_v4(),
+    manufacturer_name varchar(400) not null,
+    manufacturer_contact contact_info not null
+);
+
+create table brand_medicine(
+    brand_medicine_id uuid primary key default uuid_generate_v4(),
+    brand_medicine_name varchar(500) not null,
+    brandgeneric_medicine_id uuid not null,
+    brandmanufacturer_id uuid not null,
+    medication_type varchar(255) not null,
+    dosage varchar(255) not null,
+    price_per_unit numeric(10,6) default 0,
+    foreign key(brandmanufacturer_id) references manufacturer(manufacturer_id),
+    foreign key(brandgeneric_medicine_id) references brand_medicine(brand_medicine_id)
+);
+
+
+-- view all tables in database
+SELECT table_name
+FROM information_schema.tables
+WHERE table_type = 'BASE TABLE'
+  AND table_schema = 'public'
+  AND table_catalog = 'dbmsproject';
