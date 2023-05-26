d74ebc2331c1177a9385207c68e810c1bdc4f61b
Nawsheen Mehereen <nawsheen@iut-dhaka.edu>
2023-05-26 21:03:49 +0800

    login fixed

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,73 +1,166 @@
-import React, { Fragment, useState } from "react";
-import { Link, Redirect } from "react-router-dom";
+import React, { useEffect, useState } from 'react';
+import { Container, Row, Col, Form, Button } from 'react-bootstrap';
+import { Formik, Field, ErrorMessage } from 'formik';
+import * as Yup from 'yup';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { redirect, useNavigate } from 'react-router';
 
-import { toast } from "react-toastify";
+const CustomerRegistrationForm = () => {
+  const [disableFields, setDisableFields] = useState(false);
+  const initialValues = {
+    customer_email: '',
+    customer_password: '',
+  };
 
-const Login = () => {
-  const [inputs, setInputs] = useState({
-    email: "",
-    password: ""
+  const validationSchema = Yup.object().shape({
+    customer_email: Yup.string().email('Invalid email').required('Required'),
+    customer_password: Yup.string().required('Required'),
   });
 
-  const { email, password } = inputs;
 
-  const onChange = e =>
-    setInputs({ ...inputs, [e.target.name]: e.target.value });
-
-  const onSubmitForm = async e => {
-    e.preventDefault();
+  const checkAuthenticated = async () => {
     try {
-      const body = { customer_email:email, customer_password:password };
-      const response = await fetch(
-        "http://localhost:5000/auth/login",
-        {
-          method: "POST",
-          headers: {
-            "Content-type": "application/json"
-          },
-          body: JSON.stringify(body)
-        }
-      );
+      const res = await fetch("http://localhost:5000/auth/is-verify", {
+        method: "GET",
+        headers: { token: localStorage.token }
+      });
 
-      
-      const parseRes = await response.json();
-      
-      if (parseRes.jwtToken) {
-        console.log(body);
-        localStorage.setItem("token", parseRes.jwtToken);
-        console.log("here!");
-        toast.success("Logged in Successfully");
-      } else {
-        toast.error(parseRes);
+      const parseRes = await res.json();
+      console.log(parseRes);
+      parseRes === true ? (setDisableFields(true)
+      ) : 
+      (setIsAuthenticated(false)
+      );
+      if (parseRes) {
+        // toast.error("Logout first!");
       }
+
     } catch (err) {
       console.error(err.message);
     }
   };
 
+  useEffect(() => {
+    checkAuthenticated();
+  }, []);
+
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const navigate = useNavigate();
+
+
+
+  const handleSubmit = async (values) => {
+
+    const customer_email = values.customer_email;
+    const customer_password = values.customer_password;
+
+    try {
+      const body = {
+        customer_email,
+        customer_password,
+      };
+
+
+      const response = await fetch("http://localhost:5000/auth/login", {
+        method: "POST",
+        headers: {
+          "Content-type": "application/json"
+        },
+        body: JSON.stringify(body)
+      });
+
+      const parseRes = await response.json();
+      console.log(parseRes);
+      if (parseRes.token) {
+        localStorage.setItem("token", parseRes.token);
+        setDisableFields(true);
+        toast.success("Log in Successful, Redirecting you to dashboard", {
+          onClose: () => {
+            navigate("/dashboard"); // Redirect to dashboard
+
+          }
+        });
+
+      } else {
+        toast.error(parseRes);
+      }
+    } catch (error) {
+    }
+  };
+
+  if (isAuthenticated) {
+    toast.error("Logout first");
+  }
+
+
+
   return (
-    <Fragment>
-      <h1 className="mt-5 text-center">Login</h1>
-      <form onSubmit={onSubmitForm}>
-        <input
-          type="text"
-          name="email"
-          value={email}
-          onChange={e => onChange(e)}
-          className="form-control my-3"
-        />
-        <input
-          type="password"
-          name="password"
-          value={password}
-          onChange={e => onChange(e)}
-          className="form-control my-3"
-        />
-        <button class="btn btn-success btn-block">Submit</button>
-      </form>
-      <Link to="/register">register</Link>
-    </Fragment>
+    <Container fluid className="d-flex align-items-center justify-content-center" style={{ height: '100vh' }}>
+      <Row className="justify-content-center w-100">
+        <Col sm={8} md={6} lg={4}>
+          <div className="p-4 rounded shadow">
+            <h2 className="text-center mb-4">Login</h2>
+            <Formik
+              initialValues={initialValues}
+              validationSchema={validationSchema}
+              onSubmit={handleSubmit}
+            >
+              {(formik) => (
+                <fieldset disabled={disableFields}>
+
+                  <Form onSubmit={formik.handleSubmit}>
+
+                    <Form.Group controlId="customer_email">
+                      <Form.Label>Email:</Form.Label>
+                      <Field
+                        type="email"
+                        name="customer_email"
+                        placeholder="Enter your email"
+                        className={`form-control ${formik.errors.customer_email && formik.touched.customer_email ? 'is-invalid' : ''}`}
+                      />
+                      <ErrorMessage name="customer_email" component="div" className="invalid-feedback" />
+                    </Form.Group>
+
+                    <Form.Group controlId="customer_password">
+                      <Form.Label>Password:</Form.Label>
+                      <Field
+                        type="password"
+                        name="customer_password"
+                        placeholder="Enter your password"
+                        className={`form-control ${formik.errors.customer_password && formik.touched.customer_password ? 'is-invalid' : ''
+                          }`}
+                      />
+                      <ErrorMessage name="customer_password" component="div" className="invalid-feedback" />
+                    </Form.Group>
+
+
+
+                    <Button variant="primary" type="submit" className="mt-3 w-100">
+                      Login
+                    </Button>
+                  </Form>
+                </fieldset>
+              )}
+            </Formik>
+          </div>
+        </Col>
+      </Row>
+      <ToastContainer
+        position="top-center"
+        autoClose={1000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        type="success"
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="dark"
+      />
+    </Container>
   );
 };
 
-export default Login;
+export default CustomerRegistrationForm;
